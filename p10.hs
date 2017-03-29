b5051f1db2b0bf8472443ab9d205bd3130be4495
zach007 <zach007cn@gmail.com>
2017-03-29 13:29:13 +0000

    the same as p09 and still let (first,rest) = span (==1) [a] is a good function ,there must be another form like this !

---
+++ p10.hs
@@ -0,0 +1,6 @@
+module Encode where
+
+encode :: Eq a => [a] -> [(Int,a)]
+encode [] = []
+encode (x:xs) = let (first, rest) = span (==x) xs
+                in [(1 + length first,x) ] ++ encode rest
