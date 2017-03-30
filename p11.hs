6643b94b129f3d8a51a9b0101094fd375e6998ad
zach007 <zachcn007cn@gmail.com>
2017-03-30 00:37:42 +0000

    using type parameter, data Muti a = Multiple Int a | Single a , encodeModify :: a -> [Mutil a]

--- p11.hs
+++ p11.hs
@@ -1,10 +1,21 @@
 module EncodeModified where
-
+{--
 data Modified  = Multiple Int Char
                | Single Char
                 deriving (Show,Eq)
 
 encodeModified :: [Char] -> [Modified]
+encodeModified [] = []
+encodeModified (x:xs) = let (first, rest) = span (==x) xs
+                      in if length first == 0 then Single x : encodeModified rest else Multiple (1+length first) x : encodeModified rest
+--}
+
+--this function is support [a] ,not just Char ,by using type parameters!
+
+
+data Modified a = Multiple Int a | Single a  deriving (Show)
+
+encodeModified :: Eq a => [a] -> [Modified a]
 encodeModified [] = []
 encodeModified (x:xs) = let (first, rest) = span (==x) xs
                       in if length first == 0 then Single x : encodeModified rest else Multiple (1+length first) x : encodeModified rest
