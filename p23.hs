3a6579bfc5a6acb98aa82b35fa2a7b773526dd23
zach007 <zach007cn@gmail.com>
2017-04-06 15:24:19 +0000

    know the Random of FP, mkStdGen, random,randomR, randoms and other functions

---
+++ p23.hs
@@ -0,0 +1,15 @@
+module RandomNum where
+
+import System.Random
+
+--this is just implement the Char but not for the Int Example
+rmd_selected :: [Char] -> Int -> IO[Char]
+rmd_selected xs n = do
+      gen <- getStdGen
+      return $ take n (randomRs ('a','z') gen)
+
+superRmd :: [a] -> Int -> IO [a]
+superRmd xs n = do
+    gen <- newStdGen
+    return $ take n $ [xs !! p | p <- randomRs (0, (length xs) -1) gen]
+
