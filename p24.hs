4cb786c01847ab51ac0c74df4b02cd538617652a
zach007 <zach007cn@gmail.com>
2017-04-06 15:38:17 +0000

    IO example should konw more about IO Monad and xs compress like [xs git add p24.hs  p | p <- Random (0,(length xs)-1) (mkStdGen 100)]

---
+++ p24.hs
@@ -0,0 +1,8 @@
+module NRandomNum where
+
+import System.Random
+
+diff_select :: Int -> Int -> IO [Int]
+diff_select n bound = do
+                gen <- getStdGen
+                return $ take n $ randomRs (0,bound) (mkStdGen 100)
